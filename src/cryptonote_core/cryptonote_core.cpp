9d18aa1ed6ed507494b441d6c96486e9b7ca6309
m2049r <m2049r@monerujo.io>
2020-09-24 21:11:26 +0200

    compiles

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -52,7 +52,7 @@ using namespace epee;
 #include "ringct/rctTypes.h"
 #include "blockchain_db/blockchain_db.h"
 #include "ringct/rctSigs.h"
-#include "rpc/zmq_pub.h"
+//#include "rpc/zmq_pub.h"
 #include "common/notify.h"
 #include "hardforks/hardforks.h"
 #include "version.h"
